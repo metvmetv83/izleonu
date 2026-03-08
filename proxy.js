bfc14d8f7601468bab46490a3d00e84e2e9c61a0
metvmetv83 <metvmetv83@gmail.com>
2026-03-08 06:16:44 +0300

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -6,8 +6,8 @@ app.get('/proxy', async (req, res) => {
     const response = await fetch(targetUrl, {
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        'Referer': 'https://macizlevip315.shop/',
-        'Origin': 'https://macizlevip315.shop'
+        'Referer': 'https://izlemac467.sbs/',
+        'Origin': 'https://izlemac467.sbs'
       }
     });
 
